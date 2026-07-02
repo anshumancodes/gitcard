d87901bd89b82a9461b5ed3becc98dd2c96a9861
anshumancodes <anshumanprof01@gmail.com>
2026-07-02 22:37:52 +0530

    feat(next.config): add image remote patterns & noindex header

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+  
+  async headers() {
+    return [
+      {
+        source: "/:username",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
